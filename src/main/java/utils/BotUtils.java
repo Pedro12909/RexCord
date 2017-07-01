51396fd870750bf2db129a44f869737d5ec1aca1
DESKTOP-0VAEMFL\joaom <1160907@isep.ipp.pt>
2017-07-01 01:09:53 +0100

    Added a method that detects if a certain user which triggered an event has a certain role on the server

--- src/main/java/utils/BotUtils.java
+++ src/main/java/utils/BotUtils.java
@@ -1,8 +1,12 @@
 package utils;
 
+import java.util.List;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.impl.events.guild.channel.message
+        .MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.util.DiscordException;
 
 /**
@@ -33,8 +37,8 @@ public final class BotUtils {
     /**
      * Configuration's File Path
      */
-    public static final String DEFAULT_CONFIG_PATH =
-            System.getProperty("user.dir") + "/config/config.cfg";
+    public static final String DEFAULT_CONFIG_PATH
+            = System.getProperty("user.dir") + "/config/config.cfg";
 
     /**
      * Missing Token Error Message
@@ -45,22 +49,22 @@ public final class BotUtils {
     /**
      * Config missing Error Message
      */
-    public static final String CONFIG_NOT_FOUND_ERROR =
-            "RexCord: Config file not found. "
-                    + "Make sure it is created and located in "
-                    + "the correct directory.";
+    public static final String CONFIG_NOT_FOUND_ERROR
+            = "RexCord: Config file not found. "
+            + "Make sure it is created and located in "
+            + "the correct directory.";
 
     /**
      * Default Error Message
      */
-    public static final String ERROR_MESSAGE =
-            "RexCord: Unable to start RexCord!";
+    public static final String ERROR_MESSAGE
+            = "RexCord: Unable to start RexCord!";
 
     /**
      * RexCord terminating message
      */
-    public static final String TERMINATING_MESSAGE =
-            "RexCord: Terminating RexCord...";
+    public static final String TERMINATING_MESSAGE
+            = "RexCord: Terminating RexCord...";
 
     /**
      * Private Constructor
@@ -71,6 +75,7 @@ public final class BotUtils {
 
     /**
      * Sets a new Bot Token
+     *
      * @param newToken new Bot Token to be set
      */
     public static void setBotToken(String newToken) {
@@ -79,6 +84,7 @@ public final class BotUtils {
 
     /**
      * Gets system start time, in mm
+     *
      * @return system start time, in mm
      */
     public static long getStartTime() {
@@ -87,6 +93,7 @@ public final class BotUtils {
 
     /**
      * S system start time, in mm
+     *
      * @param startTime system start time, in mm
      */
     public static void setStartTime(long startTime) {
@@ -95,6 +102,7 @@ public final class BotUtils {
 
     /**
      * Handles the creation of a Bot Client
+     *
      * @return A new Bot Client
      */
     public static IDiscordClient createDiscordClient() {
@@ -105,6 +113,7 @@ public final class BotUtils {
 
     /**
      * Sends a text message
+     *
      * @param channel text channel
      * @param message message
      */
@@ -115,5 +124,34 @@ public final class BotUtils {
             System.out.println("RexCord: Error sending message. Got error:");
             de.printStackTrace();
         }
+    }
+
+    /**
+     * Returns the roles of a certain user that triggered an event
+     *
+     * @param event received event triggered by an user
+     * @return List of Roles the user who triggered an event has
+     */
+    private static List<IRole> getRolesUserOnEvent(MessageReceivedEvent event) {
+        return event.getGuild().getRolesForUser(event.getAuthor());
+    }
+
+    /**
+     * Checks if a user has acess to a certain role
+     *
+     * @param event received event triggered by an user
+     * @param role String with the desired role wanted to be compared
+     * @return a boolean true if has acess, false if not
+     */
+    public static boolean hasUserAcessToRoleOnEvent(MessageReceivedEvent event,
+            String role) {
+        List<IRole> userRoles = getRolesUserOnEvent(event);
+        for (int i = 0; i < userRoles.size(); i++) {
+            if (userRoles.get(i).getName().replaceFirst("@", "")
+                    .equalsIgnoreCase(role)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
