56afd12063d106533fb9d2c7e8310931f2b20173
DESKTOP-0VAEMFL\joaom <1160907@isep.ipp.pt>
2017-07-14 23:26:50 +0100

    Added all permissions, later in future better answers could be added in order to justify the reason of not having the permission fixes #24 Info command now shows the prefix being used

--- src/main/java/utils/PermissionHandler.java
+++ src/main/java/utils/PermissionHandler.java
@@ -35,7 +35,8 @@ public class PermissionHandler {
                             + "add reactions!");
                     break;
                 case ADMINISTRATOR:
-                    //Still needs management
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "administrator commands");
                     break;
                 case ATTACH_FILES:
                     BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
@@ -76,6 +77,78 @@ public class PermissionHandler {
                 case MANAGE_MESSAGES:
                     BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
                             + "manage messages!");
+                    break;
+                case MANAGE_NICKNAMES:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "manage nicknames!");
+                    break;
+                case MANAGE_PERMISSIONS:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "manage permissions!");
+                    break;
+                case MANAGE_ROLES:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "manage roles!");
+                    break;
+                case MANAGE_SERVER:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "manage server!");
+                    break;
+                case MANAGE_WEBHOOKS:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "manage webhooks!");
+                    break;
+                case MENTION_EVERYONE:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "mention everyone!");
+                    break;
+                case READ_MESSAGES:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "read messages!");
+                    break;
+                case READ_MESSAGE_HISTORY:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "read message history!");
+                    break;
+                case SEND_MESSAGES:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "send messages!");
+                    break;
+                case SEND_TTS_MESSAGES:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "send TTS messages!");
+                    break;
+                case USE_EXTERNAL_EMOJIS:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "use external emojis!");
+                    break;
+                case VIEW_AUDIT_LOG:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "view audit log!");
+                    break;
+                case VOICE_CONNECT:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "voice connect!");
+                    break;
+                case VOICE_DEAFEN_MEMBERS:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "voice defean members!");
+                    break;
+                case VOICE_MOVE_MEMBERS:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "voice move members!");
+                    break;
+                case VOICE_MUTE_MEMBERS:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "mute members!");
+                    break;
+                case VOICE_SPEAK:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "voice speak!");
+                    break;
+                case VOICE_USE_VAD:
+                    BotUtils.sendMessage(event.getChannel(), BOT_NO_ACESS
+                            + "voice use vad!");
                     break;
                 default:
                     break;
