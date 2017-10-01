806fce18e0ae54c116d6e9ab8f9e9c3345aa084c
Thales Minussi de Sousa <tminussi@gmail.com>
2017-10-01 13:15:04 -0300

    #20 - adding XML reader

--- src/main/java/model/Configuration.java
+++ src/main/java/model/Configuration.java
@@ -5,35 +5,66 @@ import java.util.Collection;
 /**
  * Created by thales minussi on 28/09/17.
  * https://github.com/tminussi
+ * Class responsible for representing the XML configuration file
  */
-public class Configuration {
+public final class Configuration {
 
+    /**
+     * token
+     */
     private String token;
 
+    /**
+     * prefix
+     */
     private String prefix;
 
+    /**
+     * collection of banned commands
+     */
     private Collection<String> bannedCommands;
 
+    /**
+     * @return token
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * @param token
+     * sets the token
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /**
+     * @return prefix
+     */
     public String getPrefix() {
         return prefix;
     }
 
+    /**
+     * @param prefix
+     * sets the prefix
+     */
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }
 
+    /**
+     * @return collection of bannedCommands
+     */
     public Collection<String> getBannedCommands() {
         return bannedCommands;
     }
 
+    /**
+     * @param bannedCommands
+     * sets bannedCommands
+     */
     public void setBannedCommands(Collection<String> bannedCommands) {
         this.bannedCommands = bannedCommands;
     }
