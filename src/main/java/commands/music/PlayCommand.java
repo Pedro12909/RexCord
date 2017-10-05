71424687bec11bcd5b380b0ed0a406b2f51e1886
Ethan Lam <elmemphis2000@gmail.com>
2017-10-05 17:20:08 -0500

    Audio from Youtube is now extracted and foundation to extract audio from other sources is now created.

--- src/main/java/commands/music/PlayCommand.java
+++ src/main/java/commands/music/PlayCommand.java
@@ -6,6 +6,7 @@ import sx.blah.discord.handle.impl.events.guild.channel.message
         .MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IVoiceChannel;
 import sx.blah.discord.util.audio.AudioPlayer;
+import utils.AudioExtractor;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -89,34 +90,73 @@ public class PlayCommand implements BotCommand {
             return;
         }
 
+        //Try to queue song given...if it fails, then try to extract audio
         try {
-            //Connection to server for music file
-            //might be rejected because of no user agent
-            URLConnection conn = new URL(args.trim()).openConnection();
-            conn.setRequestProperty("User-Agent", rexCord.USER_AGENT);
-            AudioInputStream audioInputStream
-                    = AudioSystem.getAudioInputStream(conn.getInputStream());
-
-            player.queue(audioInputStream);
-
-            String message
-                    = String.format(
-                        "Song is now queued! Your song is #%d on the queue.",
-                            player.getPlaylistSize());
-            rexCord.sendMessage(event.getChannel(), message);
-
-
-            //Start playing music if there is nothing in the playlist.
-            if (player.getPlaylistSize() == 0) {
-                player.provide();
-            }
+            queueSong(player, event, args);
         } catch (IOException e) {
             rexCord.sendMessage(event.getChannel(),
-                    "Could not retrieve song from URL.");
+                    "Trying to extract audio from link...");
+
+            //Extract audio asyncronously to prevent blocking main thread
+            Thread extractor = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        queueSong(player, event, AudioExtractor.extract(args));
+                    } catch (IOException e1) {
+                        rexCord.sendMessage(event.getChannel(),
+                                "Could not retrieve song from URL.");
+                    } catch (UnsupportedAudioFileException e1) {
+                        rexCord.sendMessage(event.getChannel(),
+                            "RexCord does not support that audio file. :(");
+                    } catch (NullPointerException e1) {
+                        //Extractor returns a null value if it could not
+                        //extract the audio from the link
+                        rexCord.sendMessage(event.getChannel(),
+                                "Could not extract audio.");
+                    }
+                }
+            });
+            extractor.start();
         } catch (UnsupportedAudioFileException e) {
+            //Audio file given is not supported
             rexCord.sendMessage(event.getChannel(),
                     "RexCord does not support that audio file. :(");
         }
 
+    }
+
+    /**
+     * Queues song for the audio player
+     * @param player main instance of the AudioPlayer
+     * @param event event triggered when command is sent
+     * @param audioLink URL linking to audio file
+     * @throws IOException thrown if connection could not be made
+     * @throws UnsupportedAudioFileException thrown if audio file linked
+     *                                       is not playable
+     */
+    private synchronized void queueSong(AudioPlayer player,
+                           MessageReceivedEvent event,
+                           String audioLink)
+            throws IOException, UnsupportedAudioFileException {
+        //Connection to server for music file
+        //might be rejected because of no user agent
+        URLConnection conn = new URL(audioLink.trim()).openConnection();
+        conn.setRequestProperty("User-Agent", rexCord.USER_AGENT);
+        AudioInputStream audioInputStream
+                = AudioSystem.getAudioInputStream(conn.getInputStream());
+
+        player.queue(audioInputStream);
+        String message
+                = String.format(
+                "Song is now queued! Your song is #%d on the queue.",
+                player.getPlaylistSize());
+        rexCord.sendMessage(event.getChannel(), message);
+
+
+        //Start playing music if there is nothing in the playlist.
+        if (player.getPlaylistSize() == 0) {
+            player.provide();
+        }
     }
 }
