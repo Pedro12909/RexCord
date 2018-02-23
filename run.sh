042846a474dde2a1d6b75aab6b16882c80d6e56f
Pedro12909 <pedro12909@hotmail.com>
2018-02-23 10:55:21 +0000

    Fixes #77: run.sh now looks for the proper jar file

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
-java -jar target/rexcord-1.0.0.jar
+#!/bin/bash
+echo ------------------------------------------------
+echo RexCord: Getting Project version from pom.xml...
+
+VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -v '^\[')
+
+if [ -z ${VERSION} ]; then
+    echo Could not get version number from pom.xml. Exiting...
+    sleep 3
+    exit
+fi
+
+echo RexCord: Found version ${VERSION}
+echo ------------------------------------------------
+
+java -jar target/rexcord_${VERSION}.jar
+
 sleep 5
